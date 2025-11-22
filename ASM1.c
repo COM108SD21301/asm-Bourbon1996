50df5f13ef04a54b2baa6b49adb974d145e7d8b1
Hoàng Văn Ba Đức-ps48249 <hoangduc1902@gmail.com>
2025-11-22 08:28:17 +0700

    Update ASM1.c

--- ASM1.c
+++ ASM1.c
@@ -2,52 +2,62 @@
 #include <math.h>
 #include <stdlib.h>//Thieu thu vien se bi canh bao ve ham Exit
 int main(){
-	int luaChon; //kiểu số nguyên
-		printf("\nChào mừng bạn đến với ASM của tôi");
-		printf("\nNhập 1: Lựa chọn chức năng Số nguyên");
-		printf("\nNhập 2: Lựa chọn chức năng tìm UCLN và BCNN");
-		printf("\nNhập 3: Lựa chọn chức năng Tính tiền quán Karaoke");
-		printf("\nNhập 4: Lựa chọn chức năng Tính tiền điện");
-		printf("\nNhập 0: Thoát chương trình");
-		printf("\nMời bạn nhập lựa chọn: ");	scanf("%d",&luaChon);
+	int luaChon, chon; //kiểu số nguyên
+		do{
+            printf("\033c");
+            printf("\nChào mừng bạn đến với ASM của tôi");
+            printf("\nNhập 1: Lựa chọn chức năng Số nguyên");
+            printf("\nNhập 2: Lựa chọn chức năng tìm UCLN và BCNN");
+            printf("\nNhập 3: Lựa chọn chức năng Tính tiền quán Karaoke");
+            printf("\nNhập 4: Lựa chọn chức năng Tính tiền điện");
+            printf("\nNhập 0: Thoát chương trình");
+            printf("\nMời bạn nhập lựa chọn: ");	scanf("%d",&luaChon);
 		switch(luaChon){
 			case 1:{
-    printf("Bạn chọn CN1\n"); // nhập vào số thực x
-    float x;
-    printf("Mời bạn nhập số x: ");
-    scanf("%f", &x);
+                do{
+                    printf("\033c");
+                    printf("Bạn chọn CN1\n"); // nhập vào số thực x
+                    float x;
+                    printf("Mời bạn nhập số x: ");
+                    scanf("%f", &x);
 
-    if (x == (int)x) {
-        // X là số nguyên
-        printf("%.0f là số nguyên\n", x);
+                    if (x == (int)x) {
+                        // X là số nguyên
+                        printf("%.0f là số nguyên\n", x);
 
-        // Kiểm tra số chính phương
-        for (int i = 1; i <= x / 2; i++) {
-            if (i * i == x) {
-                printf("%.0f là số chính phương\n", x);
-                break;
-            }
-        }
+                        // Kiểm tra số chính phương
+                        for (int i = 1; i <= x / 2; i++) {
+                            if (i * i == x) {
+                                printf("%.0f là số chính phương\n", x);
+                                break;
+                            }
+                        }
 
-        // Kiểm tra số nguyên tố
-        int xLaSoNT = 1; // giả sử x là số nguyên tố
-        for (int i = 2; i <= sqrt(x); i++) {
-            if ((int)x % i == 0) {
-                xLaSoNT = 0;
-                break;
-            }
-        }
+                        // Kiểm tra số nguyên tố
+                        int xLaSoNT = 1; // giả sử x là số nguyên tố
+                        for (int i = 2; i <= sqrt(x); i++) {
+                            if ((int)x % i == 0) { //ép x về kiểu số nguyên 
+                                xLaSoNT = 0;
+                                break;
+                            }
+                        }
 
-        if (xLaSoNT == 1) {
-            printf("%.0f là số nguyên tố\n", x);
-        } else {
-            printf("%.0f không phải số nguyên tố\n", x);
-        }
+                        if (xLaSoNT == 1) {
+                            printf("%.0f là số nguyên tố\n", x);
+                        } else {
+                            printf("%.0f không phải số nguyên tố\n", x);
+                        }
 
-    } else {
-        printf("%.2f là số thực\n", x);
-    }
-    break; 
+                    } else {
+                        printf("%.2f là số thực\n", x);
+                    }
+                    
+                    printf("Bạn có muốn tiếp tục CN1 không?\n");
+                    printf("1: Có.\n");
+                    printf("2: Không.\n");
+                    scanf("%d", &chon);
+                }while(chon != 2);
+                     break; 
 
 			}case 2:{
                 printf("Bạn chọn CN2\n");
@@ -105,6 +115,8 @@ int main(){
 				return 0;
 			}default: printf("\nYou chose wrong\n"); break;  
 		}
+        }while(luaChon != 0);
+
 	// Dừng màn hình
     system("pause"); // hoặc getchar();
 	return 0;
